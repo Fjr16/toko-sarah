e30e5cef42e275665a4921aee2af0f4636fa4a2a
ellpanggabean <fajri.fadhila@gmail.com>
2025-09-05 19:30:59 +0700

    delete sneat template asset

--- resources/views/layouts/auth/footer.blade.php
+++
@@ -1,5 +0,0 @@
-<footer class="content-footer footer bg-footer-theme">
-  <div class="container-xxl d-flex flex-wrap justify-content-between py-2 flex-md-row flex-column">
-    <h6 class="mx-auto">Copyright @ 2024</h6>
-  </div>
-</footer>
--- resources/views/layouts/auth/main.blade.php
+++ resources/views/layouts/auth/main.blade.php
@@ -178,8 +178,6 @@
 
     <!-- Core CSS -->
     <link rel="stylesheet" href="{{ asset('/assets/vendor/libs/select2/select2.css') }}" />
-    <link rel="stylesheet" href="{{ asset('/assets/vendor/css/core.css') }}" class="template-customizer-core-css" />
-    <link rel="stylesheet" href="{{ asset('/assets/vendor/css/theme-default.css') }}" class="template-customizer-theme-css" />
     <link rel="stylesheet" href="{{ asset('/assets/css/demo.css') }}" />
 
     <!-- Vendors CSS -->
